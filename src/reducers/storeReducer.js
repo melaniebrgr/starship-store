849383767c23a97b25cba65a10e85100c3b92cb9
Melanie Burger <melanie.brgr@gmail.com>
2017-02-05 20:05:09 -0500

    spread object in reducer

--- src/reducers/storeReducer.js
+++ src/reducers/storeReducer.js
@@ -1,11 +1,11 @@
-import ACTION_TYPES from '../actions/storeActions';
+import { ACTION_TYPES } from '../actions/storeActions';
 
 export default function storeReducer(state = [], { type, payload }) {
   switch (type) {
     case ACTION_TYPES.createStoreItem:
       return [
-        ...state, 
-        Object.assign({}, payload.item)
+        ...state,
+        {...payload.item}
       ];
     default:
       return state;
