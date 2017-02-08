dbdc2af5ae22aa3c1c892f1cb0c9f5f7fa1e0026
Melanie Burger <melanie.brgr@gmail.com>
2017-02-07 22:18:23 -0500

    api-url constants file

---
+++ src/constants/api-urls.js
@@ -0,0 +1,8 @@
+const SWAPI_BASE_URL = 'http://swapi.co/api/';
+const SWAPI_VEHICLES = 'vehicles/';
+const SWAPI_STARSHIPS = 'starships/';
+const SWAPI_FILMS = 'films/';
+
+export const GET_ALL_VEHICLES = `${SWAPI_BASE_URL}${SWAPI_VEHICLES}`;
+export const GET_ALL_STARSHIPS = `${SWAPI_BASE_URL}${SWAPI_STARSHIPS}`;
+export const GET_ALL_FILMS = `${SWAPI_BASE_URL}${SWAPI_FILMS}`;
