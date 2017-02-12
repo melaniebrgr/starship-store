130f7f9e9f08d0827fd5626d9e7d7270549fb585
Melanie Burger <melanie.brgr@gmail.com>
2017-02-12 18:37:04 -0500

    init Cart component

---
+++ src/containers/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+
+export default class Cart extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Starship Cart</h2>
+      </div> 
+    );
+  }
+}
