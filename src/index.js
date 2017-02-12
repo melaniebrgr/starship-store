c3a45f96a1eef47dd221e33fa7716f235a85bb1a
Melanie Burger <melanie.brgr@gmail.com>
2017-02-12 14:56:43 -0500

    initialize store in top level store prop

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+
 import configureStore from './store/configureStore';
 import App from './components/App/App';
 import './index.css';
 
-const store = configureStore();
-
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={configureStore()}>
     <App />
   </Provider>,
   document.getElementById('root')
