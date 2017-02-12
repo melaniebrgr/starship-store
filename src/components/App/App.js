4d1e0b94ba015b16315747e5df5ebbdb30405425
Melanie Burger <melanie.brgr@gmail.com>
2017-02-12 13:26:16 -0500

    make App a dumb component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Store from '../../containers/Store/Store';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Solo Inc. Starshipping</h1>
-        </header>
-        <h2>Starship Store</h2>
-        <Store />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Solo Inc. Starshipping</h1>
+      </header>
+      <h2>Starship Store</h2>
+      <Store />
+    </div>
+  );
 }
-
-export default App;
